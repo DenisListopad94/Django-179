721b04dc725e02cb6632cc24f1193714ab4fb240
denis <den.listapad@mail.ru>
2022-09-12 23:37:13 +0300

    lesson 3

--- veicle/cars/models.py
+++ veicle/cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Auto(models.Model):
+    title = models.CharField(max_length=20)
+    speed = models.FloatField()
+    description = models.TextField()
+    year_create = models.IntegerField()
+    def __str__(self):
+        return  self.title
--- veicle/cars/views.py
+++ veicle/cars/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
-all_cars = ["bmw - очень крутая машина", "lada- сделано в СССР", "volvo - очень надежная машина"]
-
+# all_cars = ["bmw - очень крутая машина", "lada- сделано в СССР", "volvo - очень надежная машина"]
+auto = Auto.objects.all()
+context = {
+        "cars": auto,
+    }
 def cars(request):
-    return render(request, 'cars.html', {"cars": all_cars})
+    return render(request, 'cars.html', context = context)
 
 def bmw(request):
-    return render(request, 'bmw.html', {"bmw": "очень крутая машина"})
+    bmw = Auto.objects.get(pk = 1)
+    return render(request, 'bmw.html', {"bmw":bmw})
 
 def lada(request):
     return render(request,'lada.html')
